db4229c4db8999e60f386350d493d969f78988b8
Miguel Henrique <miguelhp373@gmail.com>
2021-08-02 22:52:11 -0300

    Exemplo POO - Aula 02 - 02/08/2021

---
+++ Exemplo_POO/Conta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_POO
+{
+    class Conta
+    {
+        //atributos
+        public int    numero;
+        public string agencia;
+        public string nome_cliente;
+        public double saldo;
+
+        //metodos
+    public void Saque(double valor)
+        {
+
+            if (saldo > 0)
+            {
+                saldo -= valor;
+            }
+        }
+
+    public void Deposito(double valor)
+        {
+            saldo += valor;
+        }
+
+        public double ShowSaldo()
+        {
+            return Math.Round(saldo,2);
+        }
+
+    }
+}
---
+++ Exemplo_POO/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exemplo_POO
+{
+    public partial class FrMenu : Form
+    {
+        public FrMenu()
+        {
+            InitializeComponent();
+        }
+
+        Conta ct = new Conta();//instância da conta
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double valorOperacao = Convert.ToDouble(valor.Text);
+            if (tip01.Checked == true)
+            {
+                if ((valorOperacao > ct.saldo) || (ct.saldo < 1))
+                {
+                    MessageBox.Show("Erro ao Executar a Operação, Saldo Indisponivel!", "Erro");
+                }
+                else if (ct.saldo > 0)
+                {
+                    ct.Saque(valorOperacao);
+                    saldo.Text = Convert.ToString(ct.ShowSaldo());
+                }
+            }
+
+            if (tip02.Checked == true)
+            {
+                ct.Deposito(valorOperacao);
+                saldo.Text = Convert.ToString(ct.ShowSaldo());
+
+                MessageBox.Show("Depósito de R$ " + valorOperacao + " Efetuado com Sucesso!", "info");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ct.agencia = agencia.Text;
+            ct.numero = Convert.ToInt32(conta.Text);
+            ct.nome_cliente = cliente.Text;
+
+            MessageBox.Show("Conta Cadastrada com Sucesso!"
+                + "\n" + 
+                "Agência: " + ct.agencia 
+                +"\n" +
+                "Conta: " + ct.numero 
+                +"\n" +
+                "Cliente: " + ct.nome_cliente,
+                "Dados do Cliente");
+        }
+    }
+}
