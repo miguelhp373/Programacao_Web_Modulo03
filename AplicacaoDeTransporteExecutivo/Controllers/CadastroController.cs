0d5bb707c4e93fe223f74a98f6b3ebbac56828a3
Miguel Henrique <miguelhp373@gmail.com>
2021-08-29 19:23:37 -0300

    first asp.net app

---
+++ AplicacaoDeTransporteExecutivo/Controllers/CadastroController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using TransporteExecutivo.Models;
+
+namespace TransporteExecutivo.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ReceberCadastro([FromForm] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                string conteudo = string.Format("Dados do Usuário de Transporte \n" +
+                                                "Nome: {0},\n" +
+                                                "Telefone: {1}, \n" +
+                                                "Ida: {2}, \n" +
+                                                "Volta: {3}.",
+                                                usuario.nome, usuario.telefone, usuario.ida, usuario.volta);
+
+                return new ContentResult() { Content = conteudo };
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ AplicacaoDeTransporteExecutivo/Model/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace TransporteExecutivo.Models
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage = "Campo nome é obrigatório!")]
+        [MaxLength(40, ErrorMessage = "Campo nome pode ter no máximo 40 caracters!")]
+        public string nome { get; set; }
+
+        [Required(ErrorMessage = "Campo telefone é obrigatório!")]
+        [MaxLength(15, ErrorMessage = "Campo telefone pode ter no máximo 15 caracters!")]
+        public string telefone { get; set; }
+
+        [Required(ErrorMessage = "Campo ida é obrigatório!")]
+        public string ida { get; set; }
+
+        [Required(ErrorMessage = "Campo volta é obrigatório!")]
+        public string volta { get; set; }
+    }
+}
